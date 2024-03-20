0c09c46a4983016d9fc95d27dbacc1609efcd9f4
KE <36301677+Cnnyke@users.noreply.github.com>
2024-03-20 12:48:48 +0100

    articles page done

--- src/pages/Articles/index.jsx
+++ src/pages/Articles/index.jsx
@@ -1,33 +1,64 @@
 import styled from "styled-components"
 import { articleList } from "../../Data/articles"
+import { Navigate, useNavigate } from "react-router-dom"
 
 const ArticleList = styled.ul`
     display: flex;
     flex-wrap: wrap;
 `
 
+const WidthArticleItem = 240
+const HeightArticleItem = 340
+
 const ArticleItem = styled.div`
-    height: 240px;
-    width: 240px;
+    height: ${HeightArticleItem}px;
+    width: ${WidthArticleItem}px;
     display: flex;
     flex-direction: column;
     border: 2px solid black;
     margin-right: 40px;
+    background:#dfdfdf;
+
+    &:hover {
+        cursor: pointer;
+    }
+
+    &:hover .articleName {
+        text-decoration: underline;
+    }
 `
 
 const CoverArticle = styled.img`
-    height: inherit;
-    width: inherit;
+    height: ${WidthArticleItem}px;
+    width: ${WidthArticleItem}px;
+    object-fit: cover;
 `
 
+const ArticleItemText = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: ${HeightArticleItem-WidthArticleItem}px;
+`
+
+const SpanArticle = styled.span`
+    margin: 10px;
+`
+
+
 function Articles() {
+
+    let navigate = useNavigate()
+
     return (
         <ArticleList>
-            {articleList.map(({id, category, name, description, price, images}) =>
-                <ArticleItem key={id}>
+            {articleList.map(({id, name, price, images}) =>
+                <ArticleItem key={id} onClick={() => navigate(`/articles/${id}`)}>
                     <CoverArticle src={images[0]} alt="cover"/>
-                    <span>{name}</span>
-                    <span>{price}</span>
+                    <ArticleItemText>
+                        <SpanArticle className="articleName">{name}</SpanArticle>
+                        <SpanArticle style={{color: 'red'}}>{price} €</SpanArticle>
+                    </ArticleItemText>
                 </ArticleItem>
             )}
         </ArticleList>
