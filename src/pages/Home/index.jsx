c5bbc672240dc77351990fc4ad63cde2f3d63983
KE <36301677+Cnnyke@users.noreply.github.com>
2024-03-27 13:46:50 +0100

    Home debut

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,21 @@
+import styled from 'styled-components';
+
+import Shops from '../Shops';
+import Articles from '../Articles';
+
+const HomeSectionTitle = styled.h1`
+    border-bottom: 2px solid black;
+`
+
 function Home() {
-  return (
-    <div>
-      Home Works !
-    </div>
-  );
+    return (
+        <div>
+            <HomeSectionTitle>Magasins</HomeSectionTitle>
+            <Shops />
+            <HomeSectionTitle>Derniers Articles</HomeSectionTitle>
+            <Articles />
+        </div>
+    )
 }
 
 export default Home;
